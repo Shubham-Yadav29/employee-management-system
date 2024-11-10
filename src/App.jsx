fcdfa685beb8479200ec049d44efeea3039597fe
Shubham-Yadav29 <shubham2992009@gmail.com>
2024-11-10 14:28:10 +0530

    sec commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Login from './components/Auth/Login'
 import Employee from './components/Dashboard/Employee'
 const App = () => {
   return (
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,46 +1,46 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+// const Login = () => {
+//     const [username, setUsername] = useState('');
+//     const [password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('Username:', username);
-        console.log('Password:', password);
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         console.log('Username:', username);
+//         console.log('Password:', password);
 
-        setPassword('')
-        setUsername('')
-    };
+//         setPassword('')
+//         setUsername('')
+//     };
 
-    return (
-    <div className="login-container">
-        <div className="login-box">
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit}>
-            <div className="textbox">
-            <input
-                type="email"
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-            />
-            </div>
-            <div className="textbox">
-            <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-            />
-            </div>
-        <button type="submit" className="login-btn">Login</button>
-        </form>
-        </div>
-    </div>
-    );
-};
+//     return (
+//     <div className="login-container">
+//         <div className="login-box">
+//         <h2>Login</h2>
+//         <form onSubmit={handleSubmit}>
+//             <div className="textbox">
+//             <input
+//                 type="email"
+//                 placeholder="Username"
+//                 value={username}
+//                 onChange={(e) => setUsername(e.target.value)}
+//                 required
+//             />
+//             </div>
+//             <div className="textbox">
+//             <input
+//                 type="password"
+//                 placeholder="Password"
+//                 value={password}
+//                 onChange={(e) => setPassword(e.target.value)}
+//                 required
+//             />
+//             </div>
+//         <button type="submit" className="login-btn">Login</button>
+//         </form>
+//         </div>
+//     </div>
+//     );
+// };
 
-export default Login;
+// export default Login;
