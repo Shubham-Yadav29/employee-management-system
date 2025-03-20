f19ca77568958dd02e24e8e86169af324e83b15f
Shubham-Yadav29 <shubham2992009@gmail.com>
2025-03-20 10:24:23 +0530

    last commit

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -13,6 +13,48 @@ const Login = ({handleLogin}) => {
 
     return (
     <div className="login-container">
+            <div className="container-01">
+        <h2 className="title">User Data</h2>
+        <div className="table">
+            <div className="table-header">
+            <span>Name</span>
+            <span>Email</span>
+            <span>Password</span>
+            </div>
+            <div className="table-row">
+            <span>Arjun Sharma</span>
+            <span>Arjun@01.com</span>
+            <span>123</span>
+            </div>
+            <div className="table-row">
+            <span>Priya Verma</span>
+            <span>Priya@02.com</span>
+            <span>123</span>
+            </div>
+            <div className="table-row">
+            <span>Ravi Patel</span>
+            <span>Ravi@03.com</span>
+            <span>123</span>
+            </div>
+            <div className="table-row">
+            <span>Sita Reddy</span>
+            <span>Sita@04.com</span>
+            <span>123</span>
+            </div>
+            <div className="table-row">
+            <span>Aarti Kumari</span>
+            <span>Aarti@05.com</span>
+            <span>123</span>
+            </div>
+            <div className="admin-panel-01">
+            <p><strong>Admin Panel Access:</strong><br></br>
+            <p class='alag'>Email - admin@me.com <br></br> Password - 123</p>
+            </p>
+        </div>
+        
+        </div>
+    </div>
+
         <div className="login-box">
         <h2>Login</h2>
         <form onSubmit={handleSubmit}>
